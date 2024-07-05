277678f9a0d17b2bda1e20e25581fb566cb754d0
khalid <elfarajkhalid525@gmail.com>
2024-07-05 15:05:32 +0100

    commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,5 @@
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return round(celsius, 1)
@@ -28,6 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
 
