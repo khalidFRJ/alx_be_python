9ff01d41ee3b3b944aa824d23954a6208adb43b6
khalid <elfarajkhalid525@gmail.com>
2024-07-11 11:07:28 +0100

    commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self,account_balance = 0) :
+        self.account_balance = account_balance
+    
+    def deposit (self , amount):
+        self.account_balance += amount
+    
+    def withdraw (self ,amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance:.2f}")
+
+
+
