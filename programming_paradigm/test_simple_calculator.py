6c53840f4d0b9652ca06bf8ef14b79891aa7db12
khalid <elfarajkhalid525@gmail.com>
2024-07-11 18:31:08 +0100

    commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,18 +2,26 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-     
-     def test_addition(self):
-          self.assertEqual( 4 + 3 , 7 , "shoold be true ")
 
-     def test_subtraction(self):
-          self.assertEqual( 4 - 3 , 1 , "shoold be true ")
+    def setUp(self):
+        self.calc = SimpleCalculator()
 
+    def test_addition(self):
+        self.assertEqual(self.calc.add(4, 3), 7, "should be true")
 
-     def test_multiply(self):
-          self.assertEqual( 4 * 3 , 12 , "shoold be true ")
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(4, 3), 1, "should be true")
 
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12, "should be true")
 
-     def test_divide(self):
-          self.assertEqual( 4 / 3 , 1.3333 , "shoold be true ")
-          
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4, 3), 1.3333, places=4, msg="should be true")
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
