6f32901e73bf7555cf5e1b6e895d9a7adeb08e7b
khalid <elfarajkhalid525@gmail.com>
2024-07-17 10:27:09 +0100

    commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,20 +1,21 @@
-class Book :
+class Book:
     def __init__ (self , title:str , author:str , year:int):
         self.title = title
         self.author = author
         self.year = year
     
-    def __del__(self):
-        del self.title 
-        print("Deleting (title of the book)")
-    
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
-
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
+    def __del__(self):
+        del self.title 
+        print("Deleting (title of the book)")
 
-
+my_book = Book("1984", "George Orwell", 1949)
+print(my_book.__str__())
+print(my_book.__repr__())
+print(my_book.__del__())
 
