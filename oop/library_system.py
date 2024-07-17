92ffe8283612e5f494eb022ef6a87a1791cf2c18
khalid <elfarajkhalid525@gmail.com>
2024-07-17 11:31:05 +0100

    commit

---
+++ oop/library_system.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self,title:str,author:str):
+        self.title = title
+        self.author = author 
+
+class EBook(Book):
+    def __init__(self, file_size:int):
+        self.file_size = file_size
+
+
+class PrintBook (Book):
+    def __init__(self, page_count:int):
+        self.page_count = page_count
+
+class library (Book) :
+    def __init__(self, books = (Book,EBook,PrintBook)):
+        self.books = books
+
+    def add_book(self, book):
+        self.book = book 
+        book.append(Book or PrintBook or EBook)
+    def list_books(self):
+        print(f"{self.title} and {self.author} and {self.file_size} and {self.page_count}")
+
+    
+from library_system import Book, EBook, PrintBook, Library
+
