d18d15766e45e07ba03b54d2aa1bc97b28028f2e
khalid <elfarajkhalid525@gmail.com>
2024-07-18 11:45:51 +0100

    commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,59 +2,38 @@ class Book:
     def __init__(self,title:str,author:str):
         self.title = title
         self.author = author 
-
     def __str__(self):
-        return f"Book: {self.title} by {self.author}"
-
-        
+        return f"Book: {self.title} by {self.author}"     
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
-    
     def __str__(self):
         return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
-        super().__init__(title, author,)
+        super().__init__(title, author)
         self.page_count = page_count
-
-
     def __str__(self):
         return f"printBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
-
-class Library() :
+class Library:
     def __init__(self):
         self.books = []
-    
     def add_book(self, book ):
         if isinstance(book, (Book, PrintBook, EBook)):
             self.books.append(book)
-
     def list_books(self):
         for book in self.books:
             print(book)
-
-from library_system import Book, EBook, PrintBook, Library
-
-
-
 def main():
-
     my_library = Library()
-
     classic_book = Book("Pride and Prejudice", "Jane Austen")
     digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
     my_library.add_book(classic_book)
     my_library.add_book(digital_novel)
     my_library.add_book(paper_novel)
-
     my_library.list_books()
-
 if __name__ == "__main__":
     main()
 
---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,37 @@
+class shape :
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle (shape):
+    def __init__(self, length , width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self): 
+        return self.length * self.width
+    
+class Circle (shape): 
+    def __init__(self,radius , width , length):
+        super().__init__(width,length)
+        self.radius = radius
+
+
+def area(radius):
+    return math.pi * radius ** 2
+
+import math
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()
+
+
+
