1736854e5f9e3e2b16d368eabf7f7903cd926398
khalid <elfarajkhalid525@gmail.com>
2024-07-18 10:21:54 +0100

    commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,53 +3,44 @@ class Book:
         self.title = title
         self.author = author 
 
-<<<<<<< HEAD
-class EBook(Book) :
-    def __init__(self,fiel_size) :
-        self.file_size = fiel_size
-        super().__init__(title=str , author=str )
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
-=======
 class EBook(Book):
-    def __str__(self, file_size:int):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
         self.file_size = file_size
-        super().__init__(title=str , author=str )
+    
+    def __str__(self):
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-
->>>>>>> 17fb3400429441d80d2cbe5d5d7aff98ae6db1d9
 class PrintBook(Book):
-    def __str__(self, page_count:int):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
         self.page_count = page_count
 
-<<<<<<< HEAD
-class Library(Book) :
-    def __str__(self, books ):
+    def __str__(self):
+        return f"printbook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library() :
+    def __init__(self):
         self.books = []
     
-    def add_book(self, book):
-        self.book = book 
-        book.append(Book or PrintBook or EBook)
+    def add_book(self, book ):
+        if isinstance(book, (Book, PrintBook, EBook)):
+            self.books.append(book)
 
     def list_books(self):
-        print(f"{self.title} and {self.author} and {self.file_size} and {self.page_count}")
+        for book in self.books:
+            print(book)
 
 
-    
-=======
-class library(Book) :
-     def __str__(self, books):
-        self.books = []
+from library_system import Book, EBook, PrintBook, Library
 
-    def add_book(self, book):
-        self.book = book.append(Book or PrintBook or EBook)
-        
-    def list_books(self):
-        print(f"{self.title} and {self.author} and {self.file_size} and {self.page_count}")
-
-    from library_system import Book, EBook, PrintBook, Library
 
 
 def main():
+
     my_library = Library()
 
     classic_book = Book("Pride and Prejudice", "Jane Austen")
@@ -64,5 +55,8 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> 17fb3400429441d80d2cbe5d5d7aff98ae6db1d9
+
+
+
+ 
 
