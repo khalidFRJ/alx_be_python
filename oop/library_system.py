1cfeafea9d31c3e611c88a4423eef8e3e4208dd9
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-07-18 11:50:57 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,15 +1,10 @@
 class Book:
     def __init__(self, title: str, author: str):
-        self.title = title
-        
+        self.title = titlle
         self.author = author 
+        
     def __str__(self):
         return f"Book: {self.title} by {self.author}"     
-
-        self.author = author
-
-    def __str__(self):
-        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
@@ -19,40 +14,26 @@ class EBook(Book):
     def __str__(self):
         return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-
-    def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-
+        
     def __str__(self):
         return f"printBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-class Library:
-    def __init__(self):
-        self.books = []
-    def add_book(self, book ):
-        if isinstance(book, (Book, PrintBook, EBook)):
-
-    def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
+        
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book: Book):
         if isinstance(book, Book):
-            
             self.books.append(book)
+            
     def list_books(self):
         for book in self.books:
             print(book)
 
-
->>>>>>> fcca4eea05bcaed2b97fbdae8b98efc14b330bdd
 def main():
     my_library = Library()
     classic_book = Book("Pride and Prejudice", "Jane Austen")
